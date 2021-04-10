97d02f2fcca193adf9b50cef4ded5a88f025a9bb
JJaraM <jonathan.jara.morales@gmail.com>
2021-04-10 17:44:34 +0100

    fix issue caused by testing

--- src/main/java/com/jjara/gateway/Application.java
+++ src/main/java/com/jjara/gateway/Application.java
@@ -3,8 +3,6 @@ package com.jjara.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.codec.ServerCodecConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
